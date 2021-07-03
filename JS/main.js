a29896efd0e31ba5bbc6beb8dd0a0ccdf84095e1
mikesv97 <douglas.figueroa20@itca.edu.sv>
2021-07-02 18:36:54 -0600

    Creando Nav De Mi Web

--- JS/main.js
+++ JS/main.js
@@ -3,9 +3,14 @@ var mouseleave1= true;
 var mouseleave2= true;
 var click1=0;
 
-const pagina = new fullpage('#fullpage',{
-    
-});
+smoothScroll.init({
+    selector: '[data-scroll]', // Selector for links (must be a class, ID, data attribute, or element tag)
+    selectorHeader: null, // Selector for fixed headers (must be a valid CSS selector) [optional]
+    speed: 2000, // Integer. How fast to complete the scroll in milliseconds
+    easing: 'easeInOutCubic', // Easing pattern to use
+    offset: 0, // Integer. How far to offset the scrolling anchor location in pixels
+    callback: function ( anchor, toggle ) {} // Function to run after scrolling
+    });
 
 //DOCUMENT READY JQUERY
 $(document).ready(function(){
