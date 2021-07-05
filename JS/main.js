9b6b628bc5dce7a3d32baa8746068161a0f9a746
mikesv97 <douglas.figueroa20@itca.edu.sv>
2021-07-05 17:19:12 -0600

    intercambio de secciones con jquery listo

--- JS/main.js
+++ JS/main.js
@@ -1,33 +1,41 @@
 //GLOBAL VAR
+function ocultar_mostrar(){
+    var btn_menu = $("#btn-menu").val();
 
-
-
-
-
+    if(btn_menu != 0){
+        $("#menu").addClass("ocultar");
+        $("#btn-menu").val(0);
+        
+    }else{
+        $("#menu").removeClass("ocultar");
+        $("#btn-menu").val(1);
+        
+    }
+}
 
 $(document).ready(function(){
 
     var width = $(window).width();
-    
-
+    $("#section-aboutme").hide();
 
     if(width <= 992){
         $("#menu-movil-icon").on("click",function(){
-            var btn_menu = $("#btn-menu").val();
-
-            if(btn_menu != 0){
-                $("#menu").addClass("ocultar");
-                $("#btn-menu").val(0);
-                
-            }else{
-                $("#menu").removeClass("ocultar");
-                $("#btn-menu").val(1);
-                
-            }
-
-
+            
+            ocultar_mostrar();
             
         });
     }
+
+    $("#home").on("click",function(){
+        $("#section-aboutme").hide();
+        $("#section-home").show(300);
+            ocultar_mostrar();
+    });
+
+    $("#aboutme").on("click",function(){
+        $("#section-home").hide();
+        $("#section-aboutme").show(300);
+        ocultar_mostrar();
+    });
 });
 
